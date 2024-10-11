0f737e6e409be0895d44bd02fb6635cbb8992fdf
techemmydev <gabrielemmanuelmary12345@gmail.com>
2024-10-11 11:19:41 +0100

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import HeroPage from "./views/HeroPage";
-// import HeroSection from "./components/HeroSection";
 
 const App: React.FC = () => {
   return (
     <div>
       <div className="">
         <HeroPage />
-        {/* <HeroSection /> */}
       </div>
     </div>
   );
